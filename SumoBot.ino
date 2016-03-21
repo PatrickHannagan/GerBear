0b863efac308711d83d0a23adf6bc9c50b0d6643
PatrickHannagan <patrick.robert.hannagan.33@gmail.com>
2016-03-21 19:57:36 -0400

    Sumobot Initial Code

---
+++ SumoBot.ino
@@ -0,0 +1,24 @@
+//Team GerBear Sumobot Code
+//Patrick Hannagan, Dan Schmetterling, Gerry Wagner, and Branden Garret
+#include <Pushbutton.h>
+#include <QTRSensors.h>
+#include <ZumoBuzzer.h>
+#include <ZumoMotors.h>
+#include <ZumoReflectanceSensorArray.h>
+
+#define LED_PIN 13
+
+Pushbutton button(ZUMO_BUTTON);
+ZumoBuzzer buzzer;
+ZumoMotors motors;
+ZumoReflectanceSensorArray reflectanceSensors;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
